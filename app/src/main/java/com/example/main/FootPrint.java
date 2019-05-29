2aff25e6b194bddd29098679784a4c3b0a773fa7
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-05-29 16:54:16 +0900

    ㅁㅁㅁㅁㅁ

--- app/src/main/java/com/example/main/FootPrint.java
+++ app/src/main/java/com/example/main/FootPrint.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,8 +17,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
-
-
 import java.util.Calendar;
 
 public class FootPrint extends Fragment {
