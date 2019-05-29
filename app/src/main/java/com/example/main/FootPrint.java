c22817bb95718e80183b33c50c2f78ce0f4b8d40
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-05-29 16:12:43 +0900

    지도출력완료

--- app/src/main/java/com/example/main/FootPrint.java
+++ app/src/main/java/com/example/main/FootPrint.java
@@ -36,7 +36,6 @@ public class FootPrint extends Fragment {
     int year = cal.get(Calendar.YEAR);
     int month = cal.get(Calendar.MONTH);
     int day = cal.get(Calendar.DAY_OF_MONTH);
-    MapFragment mapFrag;
     public FootPrint() {
     }
 
