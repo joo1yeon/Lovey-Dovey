b2593f6edda0252c2b9436249c23ea1983ad9918
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-06-04 14:10:36 +0900

    asdfafgdasg

--- app/src/main/java/com/example/main/FootPrint.java
+++ app/src/main/java/com/example/main/FootPrint.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -17,15 +18,29 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.Calendar;
 
-public class FootPrint extends Fragment {
+import static android.content.Context.MODE_PRIVATE;
+
+public class FootPrint extends Fragment implements OnMapReadyCallback {
     ImageView btnCal;
     TextView btnToday;
     Calendar cal = Calendar.getInstance();
     int year = cal.get(Calendar.YEAR);
     int month = cal.get(Calendar.MONTH);
     int day = cal.get(Calendar.DAY_OF_MONTH);
+    GroundOverlayOptions videoMark;
+    GoogleMap gMap;
+    MapFragment mapFrag;
     public FootPrint() {
     }
 
@@ -34,6 +49,10 @@ public class FootPrint extends Fragment {
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.footprint, container, false);
         btnCal = layout.findViewById(R.id.btnCal);
         btnToday = layout.findViewById(R.id.btnToday);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MODE_PRIVATE);
+        mapFrag = (MapFragment) getFragmentManager().findFragmentByTag(R.id.map);
+        mapFrag.getMapAsync(this);
+
 
         btnCal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +77,19 @@ public class FootPrint extends Fragment {
         return layout;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.584,126.925),15));
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                videoMark=new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.marker2)).position(latLng,100f,100f);
+                gMap.addGroundOverlay(videoMark);
+            }
+        });
 
+    }
 }
