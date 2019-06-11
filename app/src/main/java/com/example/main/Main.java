e310a089eada1ac82036d33f67d5a5eb89b27766
ggoniyam <50979160+ggoniyam@users.noreply.github.com>
2019-06-11 13:39:16 +0900

    main xml

--- app/src/main/java/com/example/main/Main.java
+++ app/src/main/java/com/example/main/Main.java
@@ -3,6 +3,7 @@ package com.example.main;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,7 @@ public class Main extends Fragment {
     @Override
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.main,container,false);
+        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.main,container,false);
         /*이 부분에 원래 자바 코드의 onCreate에 있는 부분 작성!!!
             findViewByid 는 layout.findViewByID로 바꾸면 사용가능!!!!
             MainActivity.this 이런거는 getContext()로 사용가능!!!
