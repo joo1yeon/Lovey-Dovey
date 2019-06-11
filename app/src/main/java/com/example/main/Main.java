aa60ce9b333ed3816f55629231bd7fe4b5cd2107
ggoniyam <50979160+ggoniyam@users.noreply.github.com>
2019-06-11 13:45:22 +0900

    main java 00

--- app/src/main/java/com/example/main/Main.java
+++ app/src/main/java/com/example/main/Main.java
@@ -1,17 +1,27 @@
 package com.example.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends Fragment {
-    /*원래 자바 코드에서 전역변수 선언 부분*/
+    ImageView gift_Btn, profile_Btn1, profile_Btn2, storage, close;
+    TextView to_do_Btn, date;
+    View profileLayout1, profileLayout2;
 
     public Main(){}
 
@@ -19,11 +29,105 @@ public class Main extends Fragment {
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.main,container,false);
-        /*이 부분에 원래 자바 코드의 onCreate에 있는 부분 작성!!!
-            findViewByid 는 layout.findViewByID로 바꾸면 사용가능!!!!
-            MainActivity.this 이런거는 getContext()로 사용가능!!!
-        */
+
+        to_do_Btn = layout.findViewById(R.id.to_do_Btn);
+        gift_Btn = layout.findViewById(R.id.gift_Btn);
+        date = layout.findViewById(R.id.date);
+
+        profile_Btn1 = layout.findViewById(R.id.profile_Btn1);
+        profile_Btn2 = layout.findViewById(R.id.profile_Btn2);
+
+
+        //Date 날짜 계산 함수
+        doDateSystem();
+
+
+/*
+        //to_do_list 버튼 눌렀을 때 --> to_do 화면 이동
+        to_do_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), TodoActivity.class);
+                startActivity(intent);
+            }
+        });
+*/
+
+        //선물버튼 눌렀을 때 --> 선물화면 이동
+        gift_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+/*
+        //왼쪽 프로필을 누를 때 -->  정보 변경 가능한 다이얼로그 창
+        profile_Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(getContext());
+                final AlertDialog dl = dlg.create();
+                profileLayout1 = View.inflate(getContext(), R.layout.activity_profile1, null);
+                dl.setView(profileLayout1);
+                dl.show();
+
+                storage = profileLayout1.findViewById(R.id.storage);
+                storage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dl.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        //오른쪽 프로필을 누를 때 -->  정보 변경 불가능한 다이얼로그 창
+        profile_Btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(getContext());
+                final AlertDialog dl2 = dlg.create();
+                profileLayout2 = View.inflate(getContext(), R.layout.activity_profile2, null);
+                dl2.setView(profileLayout2);
+                dl2.show();
+
+                close = profileLayout2.findViewById(R.id.close);
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dl2.dismiss();
+                    }
+                });
+
+            }
+        });*/
 
         return layout;
+    }
+
+    public void doDateSystem(){
+        String start = "2018-05-28";        // 사귄 날짜 입력
+
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date beginDate = formatter.parse(start);
+            String end = formatter.format(new Date());
+            Date endDate = formatter.parse(end);
+
+            // 시간차이를 시간,분,초를 곱한 값으로 나누면 하루 단위가 나옴
+            long diff =  endDate.getTime() - beginDate.getTime();
+            long coupleDays = diff / (24 * 60 * 60 * 1000);
+
+            if(coupleDays > 1000) {
+                date.setTextSize(40);
+            }
+            date.setText (coupleDays+" 일");
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
