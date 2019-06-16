46020ac45bd80490bf074f077c6788dcafe2bcb4
ggoniyam <50979160+ggoniyam@users.noreply.github.com>
2019-06-17 07:15:57 +0900

    todolist

--- app/src/main/java/com/example/main/ListData.java
+++ app/src/main/java/com/example/main/ListData.java
@@ -2,22 +2,24 @@ package com.example.main;
 
 import android.widget.CheckBox;
 
+import java.text.Collator;
+import java.util.Comparator;
+
 public class ListData {
 
+    public boolean isChecked = false;
     public CheckBox checkBox;
     public String content;
     public String date;
 
-    /**
-     * 알파벳 이름으로 정렬
-
+    //알파벳으로 이름 정렬
     public static final Comparator<ListData> ALPHA_COMPARATOR = new Comparator<ListData>() {
         private final Collator sCollator = Collator.getInstance();
 
         @Override
         public int compare(ListData mListDate_1, ListData mListDate_2) {
-            return sCollator.compare(mListDate_1.mTitle, mListDate_2.mTitle);
+            return sCollator.compare(mListDate_1.content ,mListDate_2.content);
         }
-    };*/
+    };
 
 }
