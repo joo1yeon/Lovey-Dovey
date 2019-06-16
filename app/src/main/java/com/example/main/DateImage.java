0e8f147739b98ac8af7a75a228914b3eba830841
ghksgml <50977139+ghksgml@users.noreply.github.com>
2019-06-16 15:56:10 +0900

    데이트 xml

--- app/src/main/java/com/example/main/DateImage.java
+++ app/src/main/java/com/example/main/DateImage.java
@@ -1,9 +1,11 @@
 package com.example.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,12 @@ public class DateImage extends Fragment {
             // MainActivity에서 받아온 Resource를 ImageView에 셋팅
             imageView.setImageResource(args.getInt("imgRes"));
         }
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return view;
     }
 }
