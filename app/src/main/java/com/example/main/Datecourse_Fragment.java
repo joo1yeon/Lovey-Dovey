c181cd8b08be4c0293f75f4f97366da6975fd0c5
ghksgml <50977139+ghksgml@users.noreply.github.com>
2019-07-11 11:24:42 +0900

    즐겨찾기2

--- app/src/main/java/com/example/main/Datecourse_Fragment.java
+++ app/src/main/java/com/example/main/Datecourse_Fragment.java
@@ -39,10 +39,11 @@ public class Datecourse_Fragment extends AppCompatActivity implements View.OnCli
                 i++;
                 if (i % 2 != 0) {
                     favorite.setBackgroundResource(R.drawable.ic_star_black_24dp);
+                    Toast.makeText(Datecourse_Fragment.this, "즐겨찾기에 추가되었습니다.", Toast.LENGTH_SHORT).show();
                 } else {
                     favorite.setBackgroundResource(R.drawable.ic_star_border_black_24dp);
+                    Toast.makeText(Datecourse_Fragment.this, "즐겨찾기에서 삭제되었습니다.", Toast.LENGTH_SHORT).show();
                 }
-                Toast.makeText(Datecourse_Fragment.this, "즐겨찾기에 추가되었습니다.", Toast.LENGTH_SHORT).show();
             }
         });
 
