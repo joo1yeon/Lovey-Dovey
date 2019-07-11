972c2351f571c653ee97381ed458501a34499f27
joo1yeon <50977497+joo1yeon@users.noreply.github.com>
2019-07-12 00:53:02 +0900

    스토리 내용 테스트중

---
+++ app/src/main/java/com/example/main/StoryContentsFragment.java
@@ -0,0 +1,26 @@
+package com.example.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StoryContentsFragment extends Fragment { //하루의 스토리(사진, 글)을 보여주는 컨트롤러 객체
+    private Story mStory;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mStory = new Story();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_story, container, false);
+        return v;
+    }
+}
