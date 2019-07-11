67ac306fd86888140f500bb6bca5ac819685a4b6
ghksgml <50977139+ghksgml@users.noreply.github.com>
2019-07-11 11:02:20 +0900

    즐겨찾기

--- app/src/main/java/com/example/main/Datecourse_Fragment.java
+++ app/src/main/java/com/example/main/Datecourse_Fragment.java
@@ -10,13 +10,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Datecourse_Fragment extends AppCompatActivity implements View.OnClickListener {
 
     TextView dateInfo, dateReview, infoLine, reviewLine;
     Date_Info date_info;
-
+    ImageView favorite;
+    int i = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +30,21 @@ public class Datecourse_Fragment extends AppCompatActivity implements View.OnCli
         dateReview = findViewById(R.id.dateReview);
         infoLine = findViewById(R.id.infoLine);
         reviewLine = findViewById(R.id.reviewLine);
+        favorite = findViewById(R.id.favorite);
         date_info = new Date_Info();
+
+        favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i++;
+                if (i % 2 != 0) {
+                    favorite.setBackgroundResource(R.drawable.ic_star_black_24dp);
+                } else {
+                    favorite.setBackgroundResource(R.drawable.ic_star_border_black_24dp);
+                }
+                Toast.makeText(Datecourse_Fragment.this, "즐겨찾기에 추가되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
--- app/src/main/java/com/example/main/Datecourse_Search.java
+++ app/src/main/java/com/example/main/Datecourse_Search.java
@@ -1,22 +1,61 @@
 package com.example.main;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Datecourse_Search extends AppCompatActivity {
+
+    View background;
+    AutoCompleteTextView editText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.datecourse_search);
 
-        String[] items={"벚꽃 명소","홍대 맛집","루프탑","벚꽃 명소2","홍대 놀거리","벚꽃 보러간당"};
+        //String[] items={"벚꽃 명소","홍대 맛집","루프탑","벚꽃 명소2","홍대 놀거리","벚꽃 보러간당"};
 
-        AutoCompleteTextView editText= findViewById(R.id.editText);
+        String[] items = {"a", "hong", "loop", "ab", "hongplay", "abc"};
+        ImageButton btnHome = findViewById(R.id.btnHome);
+        background = findViewById(R.id.background);
+        final boolean[] color = {false};
 
-        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,items);
+        editText = findViewById(R.id.editText);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items);
         editText.setAdapter(adapter);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color[0] = true;
+                if (color[0] == true) {
+                    background.setBackgroundColor(Color.rgb(189, 189, 189));
+                }
+                background.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        background.setBackgroundColor(Color.rgb(255, 255, 255));
+                    }
+                });
+            }
+        });
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Datecourse_Search.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
