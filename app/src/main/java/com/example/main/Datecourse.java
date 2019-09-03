a66ae17a743812f8864534e3b2baba706dd01c99
joo1yeon <50977497+joo1yeon@users.noreply.github.com>
2019-09-03 23:48:02 +0900

    Update Datecourse.java

--- app/src/main/java/com/example/main/Datecourse.java
+++ app/src/main/java/com/example/main/Datecourse.java
@@ -22,12 +22,6 @@ public class Datecourse extends Fragment {
     @Override
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.datecourse,container,false);
-        /*이 부분에 원래 자바 코드의 onCreate에 있는 부분 작성!!!
-            findViewByid 는 layout.findViewByID로 바꾸면 사용가능!!!!
-            MainActivity.this 이런거는 getContext()로 사용가능!!!
-        */
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_date,container,false);
         ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.datecourse,container,false);
         // Fragment로 넘길 Image Resource
 
@@ -38,7 +32,6 @@ public class Datecourse extends Fragment {
         listImage.add(R.drawable.image3);
         listImage.add(R.drawable.image4);
 
-        ViewPager viewPager = layout.findViewById(R.id.date_viewPager);
         ViewPager viewPager = layout.findViewById(R.id.viewPager);
         FragmentAdapter fragmentAdapter = new FragmentAdapter(getFragmentManager());
         // ViewPager와  FragmentAdapter 연결
