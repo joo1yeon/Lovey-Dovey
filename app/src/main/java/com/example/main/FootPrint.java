be0f14a17cba5e8e23596b46891c0bf989cac822
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-09-03 23:12:51 +0900

    tab수정

--- app/src/main/java/com/example/main/FootPrint.java
+++ app/src/main/java/com/example/main/FootPrint.java
@@ -4,7 +4,6 @@ package com.example.main;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
