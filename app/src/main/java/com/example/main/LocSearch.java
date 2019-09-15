ba9ed4eeebd7a916635e07ae39326066be60c1df
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-09-16 02:31:59 +0900

    장소검색+마커찍기

--- app/src/main/java/com/example/main/LocSearch.java
+++ app/src/main/java/com/example/main/LocSearch.java
@@ -1,18 +1,33 @@
 package com.example.main;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LocSearch extends AppCompatActivity {
+    LocDBHelper myHelper;
+    SQLiteDatabase sqlDB;
     EditText edtLocation;
+    Geocoder geocoder;
     ImageView btnSearch;
     RecyclerView recyclerView;
     @Override
@@ -20,6 +35,7 @@ public class LocSearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
 
+        geocoder=new Geocoder(this);
         edtLocation=findViewById(R.id.edtSearch);
         btnSearch=findViewById(R.id.btnSearch);
         recyclerView=findViewById(R.id.recyclerView);
@@ -30,16 +46,67 @@ public class LocSearch extends AppCompatActivity {
         final ArrayList<FootContentList> locList = new ArrayList<>();
         final FootContentAdapter footContentAdapter = new FootContentAdapter(locList, getApplicationContext());
         recyclerView.setAdapter(footContentAdapter);
-
+        myHelper=new LocDBHelper(this);
+        sqlDB=myHelper.getWritableDatabase();
+//        sqlDB.execSQL("delete from location; ");
+        final Cursor cursor=sqlDB.rawQuery("select * from location;",null);
+        if(cursor.getCount()>0){
+            while(cursor.moveToNext()){
+                locList.add(new FootContentList(cursor.getString(0)));
+            }
+            footContentAdapter.notifyDataSetChanged();
+        }
         //TODO 검색버튼을 누르면 ArrayList에 검색 내역 추가
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("test","1번확인");
+
                 if(edtLocation.getText().toString()!=null){
-                    locList.add(new FootContentList(edtLocation.getText().toString()));
+                    locList.clear();
+                    Log.i("test","2번확인");
+                    sqlDB.execSQL("insert into location values ('"+edtLocation.getText().toString()+"');");
+                    while(cursor.moveToNext()){
+                        locList.add(new FootContentList(cursor.getString(0)));
+                    }
                     footContentAdapter.notifyDataSetChanged();
+                    cursor.close();
+                    sqlDB.close();
                 }
+                String str =edtLocation.getText().toString();
+                List<Address> addressList = null;
+                try{
+                    addressList = geocoder.getFromLocationName(str,1);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                Log.i("test","3번확인");
+
+                String []splitStr = addressList.get(0).toString().split(",");
+                Log.i("test","3-1"+splitStr[0]);
+                Log.i("test","3-1"+splitStr[10]);
+                Log.i("test","3-1"+splitStr[12]);
+
+                String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length() - 2);
+                double latitude =addressList.get(0).getLatitude();
+                double longitude=addressList.get(0).getLongitude();
+                Log.i("test","3-"+latitude);
+                Log.i("test","3-"+longitude);
+                Log.i("test","3-"+address);
+
+                Intent outIntent = new Intent(getApplicationContext(),FootPrint.class);
+                outIntent.putExtra("latitude", latitude);
+                outIntent.putExtra("longitude",longitude);
+                outIntent.putExtra("address",address);
+                Log.i("test","4번확인");
+                Log.i("test","4-"+latitude);
+                Log.i("test","4-"+longitude);
+                Log.i("test","4-"+address);
+
+                setResult(RESULT_OK,outIntent);
+                finish();
             }
         });
+
     }
 }
