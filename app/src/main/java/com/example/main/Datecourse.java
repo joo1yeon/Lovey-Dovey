a16c1846700febee3edcae3a736444141ceab34d
ghksgml <50977139+ghksgml@users.noreply.github.com>
2019-09-18 10:51:08 +0900

    DB주석

--- app/src/main/java/com/example/main/Datecourse.java
+++ app/src/main/java/com/example/main/Datecourse.java
@@ -1,5 +1,6 @@
 package com.example.main;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,16 +9,32 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class Datecourse extends Fragment {
 
     public Datecourse(){}
+
+    RequestQueue queue;
+    NetworkImageView imageView;
 
     @Override
     @Nullable
@@ -26,11 +43,41 @@ public class Datecourse extends Fragment {
         // Fragment로 넘길 Image Resource
 
         //ArrayList에 해당 image를 넣는다.
-        ArrayList<Integer> listImage = new ArrayList<>();
+        final ArrayList<Integer> listImage = new ArrayList<>();
         listImage.add(R.drawable.image1);
         listImage.add(R.drawable.image2);
         listImage.add(R.drawable.image3);
         listImage.add(R.drawable.image4);
+
+      /* imageView=layout.findViewById(R.id.imageView);
+
+        JsonObjectRequest jsonRequest=new JsonObjectRequest(Request.Method.GET, "http://10.0.2.2/teamProject/test.json", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String imgFile = response.getString("file");
+                            if (imgFile != null && !imgFile.equals("")) {
+                                ImageLoader imageLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+                                    @Override
+                                    public Bitmap getBitmap(String url) {
+                                        return null; }
+                                    @Override
+                                    public void putBitmap(String url, Bitmap bitmap) { }
+                                });
+                                imageView.setImageUrl("http://10.0.2.2/teamProject/" + imgFile, imageLoader);
+                            }
+                        } catch (Exception e) { }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("test", error.toString());
+            }
+        });
+        
+        queue = (RequestQueue) Volley.newRequestQueue(getContext());
+        queue.add(jsonRequest);*/
 
         ViewPager viewPager = layout.findViewById(R.id.viewPager);
         FragmentAdapter fragmentAdapter = new FragmentAdapter(getChildFragmentManager());
@@ -46,13 +93,13 @@ public class Datecourse extends Fragment {
         viewPager.setPageMargin(margin/2);
 
         // FragmentAdapter에 Fragment 추가, Image 개수만큼 추가
-        for (int i = 0; i < listImage.size(); i++) {
+       /* for (int i = 0; i < listImage.size(); i++) {
             DateImage imageFragment = new DateImage();
             Bundle bundle = new Bundle();
             bundle.putInt("imgRes", listImage.get(i));
             imageFragment.setArguments(bundle);
             fragmentAdapter.addItem(imageFragment);
-        }
+        }*/
         fragmentAdapter.notifyDataSetChanged();
         return layout;
     }
