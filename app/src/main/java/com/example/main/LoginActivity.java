7734aa01bdc3eb42b230daae0f8fe0375d6a468c
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-09-25 01:58:27 +0900

    회원가입

--- app/src/main/java/com/example/main/LoginActivity.java
+++ app/src/main/java/com/example/main/LoginActivity.java
@@ -3,12 +3,14 @@ package com.example.main;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     CheckBox btnAutoLogin;
     MyDBHelper dbHelper=new MyDBHelper(this);
     SQLiteDatabase sqlDB;
+    TextView btnJoin,btnFindID,btnFindPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,9 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnAutoLogin=findViewById(R.id.btnAutoLogin);
         sqlDB=dbHelper.getWritableDatabase();
-
+        btnJoin=findViewById(R.id.btnJoin);
+        btnFindID=findViewById(R.id.btnFindID);
+        btnFindPW=findViewById(R.id.btnFindPW);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +51,14 @@ public class LoginActivity extends AppCompatActivity {
                     finish();
                 } else
                     Toast.makeText(LoginActivity.this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
