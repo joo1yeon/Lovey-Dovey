80ca03ce143216200b6cc43604fc32dc03a088e3
ggoniyam <50979160+ggoniyam@users.noreply.github.com>
2019-10-09 01:20:20 +0900

    todo db 중첩 지움

--- app/src/main/java/com/example/main/MyDBHelper.java
+++ app/src/main/java/com/example/main/MyDBHelper.java
@@ -16,8 +16,6 @@ public class MyDBHelper extends SQLiteOpenHelper {
         db.execSQL("create table info(id varchar(50) not null, passwd varchar(50) not null);");
         db.execSQL("create table  to_do_list(num integer primary key, couple_id char(20) not null, date_check char(15), content char(50) not null, checked char(5) not null);");
 
-        db.execSQL("create table  to_do_list(num integer primary key, couple_id char(20) not null, date_check char(15), content char(100) not null, checked char(5) not null);");
-
 
         //TODO_LIST 내용
         db.execSQL("INSERT INTO to_do_list VALUES( 0,'couple0',' ','인형뽑기에서 원하는 인형 뽑아주기','false');");
