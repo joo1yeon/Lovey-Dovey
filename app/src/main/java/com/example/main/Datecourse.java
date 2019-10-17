92ae2f94f69b13be506e21b871aa467274c0209a
Seohanbyul <50897415+Seohanbyul@users.noreply.github.com>
2019-10-17 17:32:45 +0900

    volley

--- app/src/main/java/com/example/main/Datecourse.java
+++ app/src/main/java/com/example/main/Datecourse.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.android.volley.toolbox.NetworkImageView;
+
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
